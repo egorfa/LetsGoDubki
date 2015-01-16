31b4c0482f10388c4eb69b62b077f057cd2eb894
Egor <egorfalkov@mail.ru>
2015-01-16 22:31:44 +0300

    Initial commit

---
+++ app/src/main/java/com/dtd/letsgodubki/Splash_screen.java
@@ -0,0 +1,39 @@
+package com.dtd.letsgodubki;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by Egor on 16.01.2015.
+ */
+public class Splash_screen extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Thread logoTimer = new Thread()
+        {
+            public void run()
+            {
+                try {
+                    sleep(2500);
+                    startActivity(new Intent(Splash_screen.this, FirstActivity.class));
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        logoTimer.start();
+    }
+
+    @Override
+    public void onBackPressed(){
+        super.onBackPressed();
+        finish();
+    }
+}
