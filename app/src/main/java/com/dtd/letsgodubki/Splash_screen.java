87126c65846c7e41c5470c9e79b53ab8f67c23e7
Egor <egorfalkov@mail.ru>
2015-02-16 16:21:05 +0300

    Splash screen(html, gif)

--- app/src/main/java/com/dtd/letsgodubki/Splash_screen.java
+++ app/src/main/java/com/dtd/letsgodubki/Splash_screen.java
@@ -3,6 +3,7 @@ package com.dtd.letsgodubki;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 /**
  * Created by Egor on 16.01.2015.
@@ -13,19 +14,25 @@ public class Splash_screen extends Activity {
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
 
-        Thread logoTimer = new Thread()
+        setContentView(R.layout.activity_splash);
+        WebView wv = (WebView) findViewById(R.id.wv);
+        wv.loadUrl("file:///android_asset/splash.html");
+
+
+                Thread logoTimer = new Thread()
         {
             public void run()
             {
                 try {
-                    sleep(2500);//From Boris
-                    startActivity(new Intent(Splash_screen.this, FirstActivity.class));
-                    finish();
+
+                    sleep(3600);
+
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
+                startActivity(new Intent(Splash_screen.this, FirstActivity.class));
+                finish();
             }
         };
         logoTimer.start();
